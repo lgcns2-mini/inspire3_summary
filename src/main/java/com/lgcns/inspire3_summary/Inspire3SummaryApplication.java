daa96037f36c2c15845f186ad0e311eac5a77e85
aaahyunseo <hs030608@naver.com>
2025-10-23 09:41:59 +0900

    init : summary ai initialization

---
+++ src/main/java/com/lgcns/inspire3_summary/Inspire3SummaryApplication.java
@@ -0,0 +1,13 @@
+package com.lgcns.inspire3_summary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Inspire3SummaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Inspire3SummaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lgcns/inspire3_summary/Inspire3SummaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgcns.inspire3_summary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Inspire3SummaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
